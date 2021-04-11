eb2508d9204010b9a235b2dc826c75b20f7911c8
Anno2003 <idsale2002@yahoo.com>
2021-04-11 22:04:44 +0700

    add argument to mark task as done

--- cliTODO.cpp
+++ cliTODO.cpp
@@ -20,14 +20,14 @@ bool argExists(char** begin,char** end,const string& opt){
 
 int main(int argc,char** argv){
 	std::ifstream infile("todo.txt");
-	std::ofstream outfile("todo.txt",std::fstream::app);
+	std::ofstream outfile;
 	std::map<int,string[4]> listing;
 	if (argc>=2){//make sure there's argument
 		for(string line;getline(infile,line);){//load data
 			int idx=std::stoi(line.substr(0,1),nullptr,0);//ID
 			
-			listing[idx][0]=line.substr(2,0);//completion
-			listing[idx][1]=line.substr(5,0);//priority
+			listing[idx][0]=line.substr(2,1);//completion
+			listing[idx][1]=line.substr(5,1);//priority
 			listing[idx][2]=line.substr(8,14);//due
 			listing[idx][3]=line.substr(22);//task
 		}
@@ -50,7 +50,8 @@ int main(int argc,char** argv){
 			}
 		}
 		
-		if (argExists(argv,argv+2,"add")){//add argument
+		else if (argExists(argv,argv+2,"add")){//add argument
+			outfile.open("todo.txt",std::fstream::app);
 			int taskID=listing.size()+1;
 			char* task = getArg(argv,argv+argc,"add");
 			if(!task){//make sure task is specified
@@ -58,13 +59,35 @@ int main(int argc,char** argv){
 				return 0;
 			}else{
 				outfile<<taskID<<"[.](_) due:========== "<<task<<"\n";
-				outfile.close();
 				cout<<"Created task: \n|"<<taskID<<"|"<<task<<endl;
 			}
+			outfile.close();
+		}
+
+		else if(argExists(argv,argv+argc,"done")){//done argument
+			outfile.open("todo.txt",std::ofstream::trunc);
+			int taskID=std::stoi(argv[1],nullptr,0);
+			listing[taskID][0]="x";
+			for(auto key:listing){
+				string completion=listing[key.first][0];
+				string priority=listing[key.first][1];
+				string due=listing[key.first][2];
+				string task=listing[key.first][3];
+				outfile<<key.first<<"["<<completion<<"]"<<"("<<priority<<") "<<due<<" "<<task<<endl;
+			}
+			outfile.close();
+		}
+
+		else if(argExists(argv,argv+1,"prune")){//prune argument
+			//TODO:create prune to remove tasks that are done from text file
+		}
+		
+		else{
+			cout<<"Invalid argument(s)!\n";
 		}
 	}
 	else{
-		cout<<"Invalid argument!"<<std::endl;
+		cout<<"No arguments specified"<<endl;
 	}
 	
 	return 0;
